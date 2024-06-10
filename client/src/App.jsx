c3b031bb340c1382c19288556190769e393009d5
ujjwal <khannaujjwal21@gmail.com>
2024-06-10 15:48:31 +0530

    added bcrypt hashing for password

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Signup from "./Signup"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./Login"
 import Home from "./Home"
+import ForgotPassword from "./ForgotPassword"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/register" element={<Signup/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/home"  element={<Home/>}></Route>
+        <Route path="/forgot-password"  element={<ForgotPassword/>}></Route>
        </Routes>
    </BrowserRouter>
   )
--- server/index.js
+++ server/index.js
@@ -1,47 +1,96 @@
-const express = require("express")
-const cors = require("cors")
-const dotenv = require("dotenv").config()
-const connectDb = require("./config/dbConnection")
-const EmployeeModel = require("./models/Employee")
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection");
+const EmployeeModel = require("./models/Employee");
+const nodemailer = require('nodemailer');
+const bcrypt = require("bcrypt");
 
 connectDb();
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
 
-app.post('/login',(req,res) => {
-    const {email,password} = req.body;
-    EmployeeModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            if(user.password === password){
-                res.json("Success")
-            } else {
-                res.json("The password is incorrect")
+    EmployeeModel.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                return res.json({ message: "No record existed" });
             }
-        } else {
-            res.json("No record existed")
-        }
-        
-    
-    })
-})
 
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) {
+                    return res.status(500).json({ message: "Error during password comparison" });
+                }
 
-app.post('/register',(req,res) => {
-    EmployeeModel.create(req.body)
-    .then(employees => res.json(employees))
-    .catch(err =>res.json(err))
-})
+                if (isMatch) {
+                    return res.json({ message: "Success" });
+                } else {
+                    return res.json({ message: "The password is incorrect" });
+                }
+            });
+        })
+        .catch(error => {
+            return res.status(500).json({ message: "Error finding user", error });
+        });
+});
 
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
 
+    bcrypt.hash(password, 10)
+        .then(hash => {
+            EmployeeModel.create({ name, email, password: hash })
+                .then(employee => res.json({ message: "User registered successfully", employee }))
+                .catch(err => res.status(500).json({ message: "Error creating user", error: err }));
+        })
+        .catch(err => res.status(500).json({ message: "Error hashing password", error: err.message }));
+});
 
+app.post("/forgot-password", (req, res) => {
+    const { email } = req.body;
 
-app.listen(port,() => {
+    EmployeeModel.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                return res.json({ message: "User not found" });
+            }
+
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'youremail@gmail.com',
+                    pass: 'yourpassword'
+                }
+            });
+
+            var mailOptions = {
+                from: 'youremail@gmail.com',
+                to: email,
+                subject: 'Reset your password',
+                text: 'That was easy!'
+            };
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                    return res.status(500).json({ message: "Error sending email", error });
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    return res.json({ message: 'Email sent: ' + info.response });
+                }
+            });
+        })
+        .catch(error => {
+            return res.status(500).json({ message: "Error finding user", error });
+        });
+});
+
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
-
+});
